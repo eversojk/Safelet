1616143a95b6638e79d3d94424616b0a257ecd20
John Eversole <eversojk@gmail.com>
2014-04-01 02:17:34 -0400

    Rewrote create user function

--- db/server.py
+++ db/server.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python
 
-from wsgiref.simple_server import make_server
+import datetime
+import uuid
+
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
+from wsgiref.simple_server import make_server
 
 from pprint import pprint
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client['seniordesign']
+# set it to check errors
+client.write_concern = {'j': True}
+db = client['safelet']
 
 @view_config(renderer='json')
 def create_user(request):
+    coll = db['users']
     response = {
         'response'  : u'',
         'error'     : u'',
     }
 
-    json_obj = request.get('json', {})
-    if json_obj:
-        coll = db['users']
-        coll.insert(json_obj)
-        response['response'] = u'User was added successfully'
-    else:
+
+    json_obj = {}
+    try:
+        json_obj = request.json_body
+    except ValueError:
         response['error'] = u'No body included'
 
+    if json_obj:
+        try:
+            result = coll.insert(json_obj)
+            cookie = str(uuid.uuid4())
+            db['cookies'].insert({
+                'user'      : json_obj['user'],
+                'cookie'    : cookie,
+                'expires'   : datetime.datetime.now() + datetime.timedelta(hours=48),
+            })
+            response['cookie'] = cookie
+            print 'added'
+        except pymongo.errors.DuplicateKeyError:
+            response['error'] = u'User already exists'
+
+    pprint(response)
     return response
 
 if __name__ == '__main__':
