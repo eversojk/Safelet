1cbcbda229cf0f0967221631a7c482d17cc5f4b1
John Eversole <eversojk@gmail.com>
2014-04-01 08:29:49 -0400

    Pretty much working

--- Safelet/loggedinViewController.h
+++ Safelet/loggedinViewController.h
@@ -6,7 +6,9 @@
 //  Copyright (c) 2014 Team Safelet. All rights reserved.
 //
 
+#import <AudioToolbox/AudioToolbox.h>
 #import <CoreBluetooth/CoreBluetooth.h>
+#import <CoreLocation/CoreLocation.h>
 #import <UIKit/UIKit.h>
 
 @interface loggedinViewController : UIViewController
@@ -19,6 +21,12 @@
 @property CBUUID *simpleKeysChar;
 @property UIActivityIndicatorView *activity;
 @property CBPeripheral *sensorTag;
+@property BOOL test;
+@property NSMutableData *responseData;
+@property CLGeocoder *gc;
+@property CLLocation *currentLoc;
+@property CLLocationManager *locManager;
+@property CLPlacemark *placemark;
 
 - (IBAction) buttonPress:(id) sender;
 @end
