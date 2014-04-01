dde642602eac3f3da4e65d284fac024a2a233940
John Eversole <eversojk@gmail.com>
2014-04-01 03:50:08 -0400

    Fixed view adding

--- db/server.py
+++ db/server.py
@@ -124,14 +124,9 @@ if __name__ == '__main__':
     config = Configurator()
     config.add_route('api', '/api/{name}')
 
-    config.add_view(create_user, route_name='api', renderer='json')
-    config.commit()
-
-    config.add_view(login_user, route_name='api', renderer='json')
-    config.commit()
-
-    config.add_view(login_cookie, route_name='api', renderer='json')
-    config.commit()
+    config.add_view(view=create_user, name="create_user", route_name='api', renderer='json')
+    config.add_view(view=login_user, name="login_user", route_name='api', renderer='json')
+    config.add_view(view=login_cookie, name="login_cookie", route_name='api', renderer='json')
 
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
--- db/tests/display_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from pymongo import MongoClient
-from pprint import pprint
-
-client = MongoClient()
-coll = client['test']['test']
-
-pprint(list(coll.find()))
